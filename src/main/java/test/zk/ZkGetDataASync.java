f07585605fc33925dd01ca12e5402c0cc4a91965
lilong <lilong@corp.netease.com>
2018-09-10 19:30:06 +0800

    异步读取数据

--- src/main/java/test/zk/ZkGetDataASync.java
+++ src/main/java/test/zk/ZkGetDataASync.java
@@ -5,42 +5,41 @@ import org.apache.zookeeper.data.Stat;
 
 import java.util.concurrent.CountDownLatch;
 
-/**
- * 同步获取节点数据
- */
-public class ZkGetDataSync implements Watcher {
+public class ZkGetDataASync implements Watcher {
     private static CountDownLatch connectedSemophore = new CountDownLatch(1);
-    private static ZooKeeper zk = null;
-    private static Stat stat = new Stat();
+    private static ZooKeeper zk;
 
     public static void main(String[] args) throws Exception {
         String path = "/zk-book";
-        zk = new ZooKeeper("192.168.145.130:2181", 5000, new ZkGetDataSync());
+        zk = new ZooKeeper("192.168.145.130:2181", 5000, new ZkGetDataASync());
         connectedSemophore.await();
 
         zk.create(path, "123".getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
-        System.out.println(new String(zk.getData(path, true, stat)));
-        System.out.println("czxid=" + stat.getCzxid() + ", mzxid=" + stat.getMzxid() + ", version=" + stat.getVersion());
-
+        zk.getData(path, true, new IDataCallback(), null);
         zk.setData(path, "123".getBytes(), -1);
-
         Thread.sleep(Integer.MAX_VALUE);
     }
 
     @Override
     public void process(WatchedEvent event) {
-        System.out.println("Received event:" + event);
         if (Event.KeeperState.SyncConnected == event.getState()) {
             if (Event.EventType.None == event.getType() && null == event.getPath()) {
                 connectedSemophore.countDown();
             } else if (Event.EventType.NodeDataChanged == event.getType()) {
                 try {
-                    System.out.println(new String(zk.getData(event.getPath(), true, stat)));
-                    System.out.println("czxid=" + stat.getCzxid() + ", mzxid=" + stat.getMzxid() + ", version=" + stat.getVersion());
+                    zk.getData(event.getPath(), true, new IDataCallback(), null);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
+}
+
+class IDataCallback implements AsyncCallback.DataCallback{
+    @Override
+    public void processResult(int rc, String path, Object ctx, byte[] data, Stat stat) {
+        System.out.println(rc + ", " + path + ", " + new String(data));
+        System.out.println(stat.getCzxid() + ", " + stat.getMzxid() + ", " + stat.getVersion());
     }
 }
