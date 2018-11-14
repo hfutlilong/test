41cbc9b0612efcd6b42ca2ed7c25f88be88ed9d8
lilong <lilong@corp.netease.com>
2018-11-14 16:36:43 +0800

    kafka demo

---
+++ src/main/java/test/kafka/ProducerDemo.java
@@ -0,0 +1,41 @@
+package test.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.util.Properties;
+
+/**
+ * @Description 生产者
+ * @Author lilong
+ * @Date 2018-11-14 14:54
+ */
+public class ProducerDemo {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "192.168.160.128:9092");
+        properties.put("acks", "all");
+        properties.put("retries", 0);
+        properties.put("batch.size", 16384);
+        properties.put("linger.ms", 1);
+        properties.put("buffer.memory", 33554432);
+        properties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        properties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+        Producer<String, String> producer = null;
+        try {
+            producer = new KafkaProducer<String, String>(properties);
+            for (int i = 0; i < 100; i++) {
+                String msg = "Message " + i;
+                producer.send(new ProducerRecord<String, String>("HelloWorld", msg));
+                System.out.println("Sent:" + msg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        } finally {
+            producer.close();
+        }
+
+    }
+}
