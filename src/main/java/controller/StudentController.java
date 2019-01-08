e8c9f92bcee7335c9ad5ec5209732e0d3dead9f8
lilong <lilong@corp.netease.com>
2019-01-08 11:53:32 +0800

    文件上传

--- src/main/java/controller/StudentController.java
+++ src/main/java/controller/StudentController.java
@@ -1,12 +1,17 @@
 package controller;
 
 import entity.Student;
+import entity.User;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.ui.ModelMap;
+
+import java.io.File;
+import java.util.UUID;
 
 @Controller
 public class StudentController {
@@ -24,5 +29,43 @@ public class StudentController {
         model.addAttribute("id", student.getId());
 
         return "result";
+    }
+
+    @RequestMapping(value="/toUploadPage", method=RequestMethod.GET)
+    public String toUploadPage() {
+        return "ImgUploadTest";
+    }
+
+    @RequestMapping(value="/uploadImg", method = RequestMethod.POST)
+    public String uploadImg(Model model, MultipartFile file) throws Exception {
+        //上传图片的原始名称
+        String originalFilename = file.getOriginalFilename();
+        String newFilename = null;
+
+        //上传图片
+        if (file != null && StringUtils.isNotBlank(originalFilename)) {
+            //图片存储路径
+            String savePath = "E:\\SelfLearnProject\\test\\pic\\";
+            newFilename = UUID.randomUUID() + originalFilename.substring(originalFilename.lastIndexOf("."));
+            File newFile = new File(savePath + newFilename);
+            //写入磁盘
+            file.transferTo(newFile);
+        }
+
+        model.addAttribute("image", newFilename);
+        return "ImgUploadTest";
+    }
+
+    @RequestMapping("toJsonTestPage")
+    public String toJsonTestPage(Model model) {
+        return "/JsonTest";
+    }
+
+    @RequestMapping("/JsonTest")
+    @ResponseBody
+    public User JsonTest(@RequestBody User user) {
+        System.out.println("userInfo[username:" + user.getUsername() + ",password:" + user.getPassword());
+
+        return user;
     }
 }
---
+++ src/main/java/entity/User.java
@@ -0,0 +1,27 @@
+package entity;
+
+/**
+ * @Description TODO
+ * @Author lilong
+ * @Date 2019-01-05 19:30
+ */
+public class User {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
