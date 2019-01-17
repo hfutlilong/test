d4e414da2995ea47047ad60580a7d155c0e6abd2
lilong <lilong@corp.netease.com>
2019-01-17 20:27:33 +0800

    测试lambda表达式

---
+++ src/test/java/mytest/others/other/TestLambda.java
@@ -0,0 +1,56 @@
+package mytest.others.other;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * @Description TODO
+ * @Author lilong
+ * @Date 2019-01-16 19:08
+ */
+public class TestLambda {
+    public static void main(String[] args) {
+        List<UserInfo> list = makeList();
+
+//        List<Integer> userIdList = list.stream().map(UserInfo::getId).distinct().collect(Collectors.toList());
+//
+//        userIdList.forEach(System.out::println);
+
+        list.sort((UserInfo order1, UserInfo order2) -> order2.getId().compareTo(order1.getId()));
+//
+    //        list.sort(Comparator.comparing(UserInfo::getId, Comparator.nullsLast(Comparator.reverseOrder())));
+
+        list.forEach(s -> System.out.println(s.getId()));
+    }
+
+    /**
+     * 生成一个列表用于测试
+     * @return
+     */
+    private static List<UserInfo> makeList() {
+        List<UserInfo> list = new ArrayList<>();
+        UserInfo userInfo1 = new UserInfo();
+        userInfo1.setId(9);
+        userInfo1.setName("1");
+        list.add(userInfo1);
+
+        UserInfo userInfo2 = new UserInfo();
+        userInfo2.setId(6);
+        userInfo2.setName("2");
+        list.add(userInfo2);
+
+        UserInfo userInfo3 = new UserInfo();
+        userInfo3.setId(13);
+        userInfo3.setName("3");
+        list.add(userInfo3);
+
+        UserInfo userInfo4 = new UserInfo();
+        userInfo4.setId(null);
+        userInfo4.setName("4");
+        list.add(userInfo4);
+
+        return list;
+    }
+}
---
+++ src/test/java/mytest/others/other/UserInfo.java
@@ -0,0 +1,30 @@
+package mytest.others.other;
+
+/**
+ * @Description TODO
+ * @Author lilong
+ * @Date 2019-01-17 11:10
+ */
+public class UserInfo {
+
+    private Integer id;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
+
