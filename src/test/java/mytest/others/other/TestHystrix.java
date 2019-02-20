6637583956ba90b89f47131ca72aa8c5f202520f
lilong <lilong@corp.netease.com>
2019-02-20 17:26:26 +0800

    hystrix测试

--- src/test/java/mytest/others/other/TestHystrix.java
+++ src/test/java/mytest/others/other/TestHystrix.java
@@ -21,5 +21,17 @@ public class TestHystrix extends BaseTest {
             System.out.println("call times:"+(i+1)+"   result:"+result +" isCircuitBreakerOpen: "+command.isCircuitBreakerOpen());
             //本例子中5s以后，熔断器尝试关闭，放开新的请求进来
         }
+
+        System.out.println("我要休息，休息一会儿！");
+        Thread.sleep(10000);
+
+        for(int i=0;i<25;i++){
+            Thread.sleep(500);
+            HystrixCommand<String> command = new SimpleHystrix("testCircuitBreaker");
+            String result = command.execute();
+            //本例子中从第11次，熔断器开始打开
+            System.out.println("call times:"+(i+1)+"   result:"+result +" isCircuitBreakerOpen: "+command.isCircuitBreakerOpen());
+            //本例子中5s以后，熔断器尝试关闭，放开新的请求进来
+        }
     }
 }
