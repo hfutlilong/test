1eecd8da9385fe3315c89efa064a4a860b32cb00
lilong <lilong@corp.netease.com>
2019-03-21 15:32:54 +0800

    熔断测试

--- src/test/java/mytest/others/other/SimpleHystrix.java
+++ src/test/java/mytest/others/other/SimpleHystrix.java
@@ -1,19 +1,14 @@
 package mytest.others.other;
 
-/**
- * @Description TODO
- * @Author lilong
- * @Date 2019-02-19 15:59
- */
 import com.netflix.hystrix.*;
 
 import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Created by wangxindong on 2017/8/15.
  */
 public class SimpleHystrix extends HystrixCommand<String> {
-
     public SimpleHystrix(String name){
         super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("ThreadPoolTestGroup"))
                         .andCommandKey(HystrixCommandKey.Factory.asKey("testCommandKey"))
@@ -23,10 +18,9 @@ public class SimpleHystrix extends HystrixCommand<String> {
                                         .withCircuitBreakerEnabled(true)//默认是true，本例中为了展现该参数
                                         .withCircuitBreakerForceOpen(false)//默认是false，本例中为了展现该参数
                                         .withCircuitBreakerForceClosed(false)//默认是false，本例中为了展现该参数
-                                        .withCircuitBreakerErrorThresholdPercentage(5)//(1)错误百分比超过5%
+                                        .withCircuitBreakerErrorThresholdPercentage(10)//(1)错误百分比超过10%
                                         .withCircuitBreakerRequestVolumeThreshold(10)//(2)10s以内调用次数10次，同时满足(1)(2)熔断器打开
                                         .withCircuitBreakerSleepWindowInMilliseconds(5000)//隔5s之后，熔断器会尝试半开(关闭)，重新放进来请求
-//                                .withExecutionTimeoutInMilliseconds(1000)
                         )
                         .andThreadPoolPropertiesDefaults(
                                 HystrixThreadPoolProperties.Setter()
@@ -38,18 +32,24 @@ public class SimpleHystrix extends HystrixCommand<String> {
 
     @Override
     protected String run() throws Exception {
-        Random rand = new Random();
-        //模拟错误百分比(方式比较粗鲁但可以证明问题)
-        if(1==rand.nextInt(2)){
-//            System.out.println("make exception");
-            throw new Exception("make exception");
+        int i = TestHystrix.ai.getAndIncrement();
+        // 10s内达到熔断条件
+        if (i < 10) {
+            Thread.sleep(500);
+            throw new NullPointerException();
         }
-        return "running:  ";
+
+        Thread.sleep(500);
+        return "success";
     }
 
     @Override
     protected String getFallback() {
-//        System.out.println("FAILBACK");
-        return "fallback: ";
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        return "fail";
     }
 }
--- src/test/java/mytest/others/other/TestHystrix.java
+++ src/test/java/mytest/others/other/TestHystrix.java
@@ -4,34 +4,43 @@ import com.netflix.hystrix.HystrixCommand;
 import mytest.base.BaseTest;
 import org.testng.annotations.Test;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 /**
  * @Description TODO
  * @Author lilong
  * @Date 2019-02-19 17:21
  */
 public class TestHystrix extends BaseTest {
+    public static final AtomicInteger ai = new AtomicInteger();
 
     @Test
     public void testCircuitBreaker() throws Exception{
-        for(int i=0;i<25;i++){
-            Thread.sleep(500);
+        for(int i = 0; i < 20; i++) {
             HystrixCommand<String> command = new SimpleHystrix("testCircuitBreaker");
             String result = command.execute();
             //本例子中从第11次，熔断器开始打开
-            System.out.println("call times:"+(i+1)+"   result:"+result +" isCircuitBreakerOpen: "+command.isCircuitBreakerOpen());
-            //本例子中5s以后，熔断器尝试关闭，放开新的请求进来
+            System.out.println("call times:"+ i +"   result:" + result + " isCircuitBreakerOpen: " + command.isCircuitBreakerOpen());
         }
 
-        System.out.println("我要休息，休息一会儿！");
-        Thread.sleep(10000);
+        // 5s 之后熔断器半开
+        Thread.sleep(8000);
+        System.out.println("===============================");
 
-        for(int i=0;i<25;i++){
-            Thread.sleep(500);
+        for(int i = 0; i < 20; i++) {
             HystrixCommand<String> command = new SimpleHystrix("testCircuitBreaker");
             String result = command.execute();
             //本例子中从第11次，熔断器开始打开
-            System.out.println("call times:"+(i+1)+"   result:"+result +" isCircuitBreakerOpen: "+command.isCircuitBreakerOpen());
-            //本例子中5s以后，熔断器尝试关闭，放开新的请求进来
+            System.out.println("call times:"+ i +"   result:" + result + " isCircuitBreakerOpen: " + command.isCircuitBreakerOpen());
+        }
+
+        System.out.println("===============================");
+        ai.set(0);
+        for(int i = 0; i < 20; i++) {
+            HystrixCommand<String> command = new SimpleHystrix("testCircuitBreaker");
+            String result = command.execute();
+            //本例子中从第11次，熔断器开始打开
+            System.out.println("call times:"+ i +"   result:" + result + " isCircuitBreakerOpen: " + command.isCircuitBreakerOpen());
         }
     }
 }
