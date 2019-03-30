4ab72b5e3b163427bf5607e49ecebe910c07ea28
lilong <lilong@corp.netease.com>
2019-03-30 19:55:16 +0800

    redis分布式锁

--- src/main/java/utils/lock/RedisLock.java
+++ src/main/java/utils/lock/RedisLock.java
@@ -1,5 +1,6 @@
 package utils.lock;
 
+import org.apache.commons.lang3.StringUtils;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
@@ -12,7 +13,7 @@ public class RedisLock {
     /**
      * 每个线程持有自己的uuid，用于区分不同的锁
      */
-    private static final ThreadLocal<String> threadLocalUuid = ThreadLocal.withInitial(() -> UUID.randomUUID().toString());
+    private static final ThreadLocal<String> identifier = ThreadLocal.withInitial(() -> UUID.randomUUID().toString());
 
     private static final String LOCK_MSG = "OK";
     private static final Long UNLOCK_MSG = 1L;
@@ -20,7 +21,7 @@ public class RedisLock {
     /**
      * 锁过期时间默认值
      */
-    private static final int DEFAULT_EXPIRE_TIME = 60 * 1000;
+    private static final int DEFAULT_EXPIRE_TIME = 60;
 
     /**
      * 阻塞式获取锁的间隔时间
@@ -31,7 +32,6 @@ public class RedisLock {
     private static String REDIS_IP = "192.168.160.128";
     private static int REDIS_PORT = 6379;
     private static JedisPool jedisPool = new JedisPool(new JedisPoolConfig(), REDIS_IP, REDIS_PORT);
-
 
     // 加锁的键
     private String lockKey;
@@ -44,36 +44,54 @@ public class RedisLock {
     }
 
 
-
     /*******************************************
      * 锁对外提供的使用方法
      *******************************************/
+
+    /**
+     * 阻塞式锁，尝试获取锁、直到成功
+     * @throws InterruptedException
+     */
     public void lock() throws InterruptedException {
-        lock(lockKey, threadLocalUuid.get());
+        lock(lockKey, identifier.get());
     }
 
-    public void lock(int timeout) throws InterruptedException {
-        lock(lockKey, threadLocalUuid.get(), timeout);
-    }
-
+    /**
+     * 非阻塞式锁，只尝试一次，无论是否获取到锁都返回
+     * @return
+     * @throws InterruptedException
+     */
     public boolean tryLock() throws InterruptedException {
-        return tryLock(lockKey, threadLocalUuid.get());
+        return tryLock(lockKey, identifier.get());
+    }
+
+    /**
+     * 阻塞式锁，在超时时间内尝试获取锁，默认100ms尝试一次
+     * @param timeout
+     * @return
+     * @throws InterruptedException
+     */
+    public boolean tryLock(int timeout) throws InterruptedException {
+        return tryLock(lockKey, identifier.get(), timeout);
     }
 
     public boolean unlock() {
-        return unlock(lockKey, threadLocalUuid.get());
+        return unlock(lockKey, identifier.get());
     }
 
     public boolean isLocked() {
         Jedis jedis = jedisPool.getResource();
-        String result = jedis.get(lockKey);
-        jedis.close();
-        return result != null;
+        String redisVal = jedis.get(lockKey);
+        return redisVal != null && redisVal.equals(identifier.get());
     }
 
     public void flushAll() {
         Jedis jedis = jedisPool.getResource();
         jedis.flushAll();
+    }
+
+    public void shutdown() {
+        jedisPool.destroy();
     }
 
 
@@ -89,15 +107,17 @@ public class RedisLock {
         }
     }
 
-    private void lock(String key, String value, int timeout) throws InterruptedException {
+    private boolean tryLock(String key, String value, int timeout) throws InterruptedException {
         Jedis jedis = jedisPool.getResource();
 
         while (timeout >= 0) {
             if (setLockToRedis(key, value, jedis)) {
-                return;
+                return true;
             }
             timeout -= DEFAULT_SLEEP_TIME;
         }
+
+        return false;
     }
 
     private boolean tryLock(String key, String value) throws InterruptedException {
@@ -116,10 +136,8 @@ public class RedisLock {
         }
 
         // 重入
-        params = new SetParams();
-        params.xx().ex(DEFAULT_EXPIRE_TIME); // setNx，带超时时间
-        result = jedis.set(key, value, params);
-        if (LOCK_MSG.equals(result)) {
+        String redisVal = jedis.get(key);
+        if (StringUtils.isNotBlank(redisVal) && redisVal.equals(value)) {
             counter++;
             jedis.close();
             return true;
@@ -130,8 +148,15 @@ public class RedisLock {
     }
 
     private boolean unlock(String key, String value) {
+        if (counter > 0) {
+            counter--;
+            return true;
+        }
+
+
         Jedis jedis = jedisPool.getResource();
 
+        // 只有缓存的value和方法入参value相等时才删除缓存，保证只有加锁的线程有权限解锁
         String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end";
         Object result = jedis.eval(script, Collections.singletonList(key), Collections.singletonList(value));
 
--- src/test/java/mytest/redis/RedisLockTest.java
+++ src/test/java/mytest/redis/RedisLockTest.java
@@ -1,8 +1,8 @@
 package mytest.redis;
 
-import mytest.base.BaseTest;
-import org.testng.annotations.Test;
 import utils.lock.RedisLock;
+
+import java.util.concurrent.*;
 
 
 /**
@@ -10,17 +10,160 @@ import utils.lock.RedisLock;
  * @Author lilong
  * @Date 2019-03-27 20:38
  */
-public class RedisLockTest extends BaseTest {
-    private RedisLock redisLock = new RedisLock();
+public class RedisLockTest {
+    public static void main(String[] args) {
+        RedisLock redisLock = new RedisLock("testLock");
 
-    @Test
-    public void testLock() {
+        ExecutorService threadPool = Executors.newFixedThreadPool(3);
+        CountDownLatch countDownLatch = new CountDownLatch(3);
+
+        for (int i = 0; i < 1; i++) {
+            threadPool.execute(() -> {
+                try {
+                    testBlockedLock(redisLock);
+                    System.out.println("######### " + Thread.currentThread().getId() + " 馍馍片");
+                    countDownLatch.countDown();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+        }
+
         try {
-            redisLock.lock("业务名+方法名", "ip+线程");
+            countDownLatch.await();
         } catch (InterruptedException e) {
             e.printStackTrace();
+        }
+
+        System.out.println("Game Over!");
+        threadPool.shutdown();
+        redisLock.flushAll(); // 清空缓存
+    }
+
+    /**
+     * 阻塞式锁
+     *
+     * @param redisLock
+     * @throws InterruptedException
+     */
+    private static void testBlockedLock(RedisLock redisLock) throws InterruptedException {
+        System.out.println("######### " + Thread.currentThread().getId() + " 加锁中...");
+        redisLock.lock();
+        boolean isLocked = redisLock.isLocked();
+
+        try {
+            System.out.println("######### " + Thread.currentThread().getId() + (isLocked ? " 加锁成功！" : " 加锁失败！"));
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
         } finally {
-            redisLock.unlock("业务名+方法名", "ip+线程");
+            if (redisLock.unlock()) {
+                System.out.println("######### " + Thread.currentThread().getId() + " 解锁成功");
+            } else {
+                System.out.println("######### " + Thread.currentThread().getId() + " 解锁失败");
+            }
+        }
+    }
+
+    /**
+     * 非阻塞式锁
+     *
+     * @param redisLock
+     * @throws InterruptedException
+     */
+    private static void testNonBlockingLock(RedisLock redisLock) throws InterruptedException {
+        System.out.println("######### " + Thread.currentThread().getId() + " 加锁中...");
+        boolean isLocked = redisLock.tryLock();
+
+        try {
+            System.out.println("######### " + Thread.currentThread().getId() + (isLocked ? " 加锁成功！" : " 加锁失败！"));
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (redisLock.unlock()) {
+                System.out.println("######### " + Thread.currentThread().getId() + " 解锁成功");
+            } else {
+                System.out.println("######### " + Thread.currentThread().getId() + " 解锁失败");
+            }
+        }
+    }
+
+    /**
+     * 带超时时间的阻塞式锁
+     *
+     * @param redisLock
+     * @throws InterruptedException
+     */
+    private static void testBlockedLockWithTimeout(RedisLock redisLock) throws InterruptedException {
+        System.out.println("######### " + Thread.currentThread().getId() + " 加锁中...");
+        boolean isLocked = redisLock.tryLock(1000);
+
+        try {
+            System.out.println("######### " + Thread.currentThread().getId() + (isLocked ? " 加锁成功！" : " 加锁失败！"));
+            Thread.sleep(3000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (redisLock.unlock()) {
+                System.out.println("######### " + Thread.currentThread().getId() + " 解锁成功");
+            } else {
+                System.out.println("######### " + Thread.currentThread().getId() + " 解锁失败");
+            }
+        }
+    }
+
+    /**
+     * 重入锁
+     *
+     * @param redisLock
+     * @throws InterruptedException
+     */
+    private static void testReentrantLock(RedisLock redisLock) {
+        int i = 1;
+        try {
+            System.out.println("######### " + Thread.currentThread().getId() + " 第" + i +  "次加锁中...");
+            redisLock.lock();
+            boolean isLocked = redisLock.isLocked();
+            System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" +(isLocked ? "加锁成功！" : "加锁失败！"));
+            Thread.sleep(1000);
+
+            i++;
+            System.out.println("######### " + Thread.currentThread().getId() + " 第" + i +  "次加锁中...");
+            redisLock.lock();
+            isLocked = redisLock.isLocked();
+            System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" +(isLocked ? "加锁成功！" : "加锁失败！"));
+            Thread.sleep(1000);
+
+            i++;
+            System.out.println("######### " + Thread.currentThread().getId() + " 第" + i +  "次加锁中...");
+            redisLock.lock();
+            isLocked = redisLock.isLocked();
+            System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" +(isLocked ? "加锁成功！" : "加锁失败！"));
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            i = 1;
+            if (redisLock.unlock()) {
+                System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" + " 解锁成功");
+            } else {
+                System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" + " 解锁失败");
+            }
+
+            i++;
+            if (redisLock.unlock()) {
+                System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" + " 解锁成功");
+            } else {
+                System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" + " 解锁失败");
+            }
+
+            i++;
+            if (redisLock.unlock()) {
+                System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" + " 解锁成功");
+            } else {
+                System.out.println("######### " + Thread.currentThread().getId() + " 第" + i + "次" + " 解锁失败");
+            }
         }
     }
 }
